95caf6e3a110ffd35f1ad3accd49491d81c0f638
bramwell simiyu <simiyub.engineer@gmail.com>
2022-04-27 12:49:09 +0100

    no real change just trigger for jenkins

--- src/test/java/tech/topskills/weatherapi/WeatherApiApplicationTests.java
+++ src/test/java/tech/topskills/weatherapi/WeatherApiApplicationTests.java
@@ -1,7 +1,12 @@
 package tech.topskills.weatherapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class WeatherApiApplicationTests {
